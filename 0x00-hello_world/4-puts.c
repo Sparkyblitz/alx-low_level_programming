eb0f1eff25b5413b075e904315acd64728b1c1c1
sparkyblitz <uchigozie400@gmail.com>
2022-09-11 10:59:27 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>  
+/**  
+* main - print the string in the put function
+*
+* Desccription: using th main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/ 
+int main(void)
+{
+	puts(*\"Programming is like a multilingual puzzle");
+	return (0);
+}
