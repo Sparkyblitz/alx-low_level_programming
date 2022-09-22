bd832b85d316c2519dc74ad0a5a0fd5c22d57100
sparkyblitz <uchigozie400@gmail.com>
2022-09-22 11:08:54 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,11 +6,14 @@
  * @src: char string
  * Return: pointer to resulting string `dest`
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int i, c;
 
-	for (i = 0; dest[i] != '\0'; c++)
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (c = 0; src[c] != '\0'; c++)
 	{
 		dest[i] = src[c];
 		i++;
