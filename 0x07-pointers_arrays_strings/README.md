a8bed124c241fcbf0518e8e4a0a698af1e269ba5
sparkyblitz <uchigozie400@gmail.com>
2022-09-26 10:48:48 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+ This is about C pointers and also involves declaring a pointer to a pointer and acessing the stored value.
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *strpbrk(char *s, char *accept);
+char *strstr(char *haystack, char *neddle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+
+#endif
